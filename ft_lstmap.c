7a8a262261901cb32f36642ea7e8846d4d94d498
Antoine Boisvert <anboisve@c1r1p4.42quebec.com>
2022-11-04 07:33:51 -0400

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: anboisve <anboisve@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 01:12:24 by anboisve          #+#    #+#             */
-/*   Updated: 2022/11/04 07:17:05 by anboisve         ###   ########.fr       */
+/*   Updated: 2022/11/04 07:25:54 by anboisve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	list = NULL;
 	while (lst != NULL)
 	{
-		new_tmp = ft_lstnew(f(lst->content));
+		new_tmp = ft_lstnew((*f)(lst->content));
 		if (!new_tmp)
 		{
 			ft_lstclear(&list, del);
